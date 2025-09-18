d908b18b3f372b6abd1f8f6222f65d1178eab109
Mouad Fakhri <mouad.fakhri@journee.ai>
2025-09-18 16:32:36 +0100

    modify mirror_index (#16)

--- core/atomic_components/condition_handler.py
+++ core/atomic_components/condition_handler.py
@@ -17,27 +17,13 @@ def _get_emo_avg(idx=6):
     return emo_avg
 
 
-# def _mirror_index(index, size, period=2):
-#     turn = index // size
-#     res = index % size
-#     if turn % period == 0:
-#         return res
-#     else:
-#         return size - res - 1
-
 def _mirror_index(index, size, period=2):
-    # Calculate period length (going up and down)
-    period = (size - 1) * 2
-
-    # Get position within one period
-    normalized_idx = index % period
-
-    # If in first half, return the index directly
-    if normalized_idx < size:
-        return normalized_idx
+    turn = index // size
+    res = index % size
+    if turn % period == 0:
+        return res
     else:
-        # If in second half, return the mirrored index
-        return period - normalized_idx - 1
+        return size - res - 1
 
 
 class ConditionHandler:
